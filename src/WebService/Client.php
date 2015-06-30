8bd919d9d829a1f25e01895cdead97c5247aacb7
Gregory Oschwald <goschwald@maxmind.com>
2015-06-30 12:03:29 -0700

    update version number

--- src/WebService/Client.php
+++ src/WebService/Client.php
@@ -48,7 +48,7 @@ class Client implements ProviderInterface
     private $client;
     private static $basePath = '/geoip/v2.1';
 
-    const VERSION = 'v2.3.0';
+    const VERSION = 'v2.3.1';
 
     /**
      * Constructor.
