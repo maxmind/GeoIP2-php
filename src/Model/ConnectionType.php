5f08629fa56335a87330108383a28ec1ca921edb
Gregory Oschwald <goschwald@maxmind.com>
2023-07-26 12:48:36 -0700

    Document new Satellite connection type value

--- src/Model/ConnectionType.php
+++ src/Model/ConnectionType.php
@@ -10,8 +10,8 @@ use GeoIp2\Util;
  * This class provides the GeoIP2 Connection-Type model.
  *
  * @property-read string|null $connectionType The connection type may take the
- *     following values: "Dialup", "Cable/DSL", "Corporate", "Cellular".
- *     Additional values may be added in the future.
+ *     following values: "Dialup", "Cable/DSL", "Corporate", "Cellular", and
+ *     "Satellite". Additional values may be added in the future.
  * @property-read string $ipAddress The IP address that the data in the model is
  *     for.
  * @property-read string $network The network in CIDR notation associated with
